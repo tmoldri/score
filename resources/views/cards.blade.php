c2e72e3b395393d4a705159b58986f7184da9251
Tauno Möldri <tmoldri.tartu@gmail.com>
2018-02-01 13:35:04 +0200

    Typo in text

--- resources/views/cards.blade.php
+++ resources/views/cards.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>What is your score in life?</h1>
+    <h1>What is your score of life?</h1>
     <p>Select card and check boxes on things you have done in your life.</p><br><br>
     <div class="row">
         @foreach ($cards as $card)
